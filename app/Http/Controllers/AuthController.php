79532f6159937d8f03678a4a00656374bc79bdbd
Олег Сайфутдинов <zeusfly@mail.ru>
2026-04-24 16:44:15 +0300

    Created AuthController & UserSeeder

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        
+        $user = User::where('email', $request->input('email'))->first();
+        if (!$user || !Hash::check($request->input('password'), $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken('auth_token')->plainTextToken]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/login', [AuthController::class, 'login']);
 Route::resource('categories', CategoryController::class)->only(['index']);
 Route::resource('products', ProductController::class)->middlewareFor(['store', 'update', 'destroy'], 'auth:sanctum');
